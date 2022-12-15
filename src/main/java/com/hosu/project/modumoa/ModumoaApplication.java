d298fa82272da1b66931fb6f4442cf416ffd2af8
hosu <hojune0904@gmail.com>
2022-12-15 16:19:02 +0900

    initial commit - project setting

---
+++ src/main/java/com/hosu/project/modumoa/ModumoaApplication.java
@@ -0,0 +1,13 @@
+package com.hosu.project.modumoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModumoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModumoaApplication.class, args);
+	}
+
+}
